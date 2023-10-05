3796b6419ca6721dbbdc2b4ffad12c73e3e592d6
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-05 22:08:48 +0300

    i

--- lib/widgets/widgts.dart
+++ lib/widgets/widgts.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nike_ecommerce/constants/colors.dart';
+import 'package:nike_ecommerce/screens/details_screen.dart';
 
 /// remove blue light
 class MyBehavior extends ScrollBehavior {
@@ -22,41 +24,51 @@ SizedBox cateories() {
             itemBuilder: (BuildContext context, int index) {
               return Column(
                 children: [
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    height: 100,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: const Color.fromARGB(255, 202, 200, 200),
-                        gradient: const LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Color(0xffF9F9F9),
-                            Color(0xffF1F1F1),
-                          ],
-                        )),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Column(
-                              children: [
-                                const Text(
-                                  'NIKE',
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const Text('Air max 1 '),
-                                addToCardButton()
-                              ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const DetailsScreen()));
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(5),
+                      height: 100,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: const Color.fromARGB(255, 202, 200, 200),
+                          gradient: const LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xffF9F9F9),
+                              Color(0xffF1F1F1),
+                            ],
+                          )),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    'NIKE',
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const Text('Air max 1 '),
+                                  addToCardButton()
+                                ],
+                              ),
                             ),
-                          ),
-                          const Image(image: AssetImage('assets/blueshoe.png'))
-                        ]),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: const Image(
+                                  image: AssetImage('assets/blueshoe.png')),
+                            )
+                          ]),
+                    ),
                   ),
                 ],
               );
@@ -71,8 +83,7 @@ TextButton addToCardButton() {
         height: 30,
         width: 80,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: const Color(0xffFFB301)),
+            borderRadius: BorderRadius.circular(8), color: AppColors.mainamber),
         child: const Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
@@ -87,5 +98,24 @@ TextButton addToCardButton() {
             )
           ],
         )),
+  );
+}
+
+Row titleAndSeeMoreHead(String catgoriesNameHeads) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(
+        catgoriesNameHeads,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      const Text(
+        'see more',
+        style: TextStyle(
+          fontSize: 15,
+          color: AppColors.mainamber,
+        ),
+      ),
+    ],
   );
 }
