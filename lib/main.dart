4600750d77e239a632bcac8af2bc30556a6465de
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-06 12:23:44 +0300

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
-import 'package:nike_ecommerce/screens/home_screen.dart';
 import 'package:nike_ecommerce/screens/navbar.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return NavBar();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: NavBar());
   }
 }
