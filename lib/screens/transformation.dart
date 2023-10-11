50ded6eca9e6bd8d0dd0c00ac4d9337d02eb668f
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-11 15:52:17 +0300

    e

--- lib/screens/transformation.dart
+++ lib/screens/transformation.dart
@@ -85,64 +85,6 @@ class _TransformmationFunctionState extends State<TransformmationFunction>
   void setImageSide() {
     if (dragPosition <= 90 || dragPosition >= 270) {
       isleft = true;
-    } else {
-      isleft = true;
     }
-  }
-}
-
-class OnBoarding extends StatefulWidget {
-  const OnBoarding({Key? key}) : super(key: key);
-
-  @override
-  State<OnBoarding> createState() => _OnBoardingState();
-}
-
-class _OnBoardingState extends State<OnBoarding> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-                alignment: Alignment.bottomCenter,
-                child: buyingButtonWihOtShoppingIcon(context)),
-          ),
-          Image.asset('assets/BG.png'),
-          Positioned(
-            bottom: 580,
-            left: 180,
-            child: Container(
-                height: 80,
-                width: 80,
-                child: Image.asset(
-                  'assets/nikeblack.png',
-                  fit: BoxFit.fill,
-                )),
-          ),
-          const Expanded(
-            flex: 1,
-            child: TransformmationFunction(
-              urlright: 'assets/onboarding.png',
-              urlleft: 'assets/onboarding1.png',
-            ),
-          ),
-          Positioned(
-              bottom: 130,
-              left: 60,
-              right: 60,
-              child: Container(
-                height: 60,
-                width: 60,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: Image.asset('assets/360.png'),
-              )),
-        ]),
-      ),
-    );
   }
 }
