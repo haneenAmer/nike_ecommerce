f775636e1ddf961bc00dea450557bcbcbb8d745e
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-11 15:52:22 +0300

    i

---
+++ lib/screens/sign_in_wih_google/google_sigin_api.dart
@@ -0,0 +1,6 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSigninApi {
+  static final googleSignIn = GoogleSignIn();
+  static Future<GoogleSignInAccount?> login() => googleSignIn.signIn();
+}
