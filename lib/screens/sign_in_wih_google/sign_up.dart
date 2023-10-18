976ae92c40185273a714ae75d49ec2d9210c8f8d
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-18 10:28:16 +0300

    e

--- lib/screens/sign_in_wih_google/sign_up.dart
+++ lib/screens/sign_in_wih_google/sign_up.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nike_ecommerce/screens/home_screen.dart';
 import 'package:nike_ecommerce/screens/sign_in_wih_google/google_sigin_api.dart';
+import 'package:nike_ecommerce/screens/sign_in_wih_google/user_profile_screen.dart';
 
-Future<void> signIn() async {
+Future<GoogleSignInAccount?> signUp(BuildContext context) async {
   try {
     // Call the asynchronous function with await
     GoogleSignInAccount? googleUser = await GoogleSigninApi.login();
+
+    if (googleUser == null) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Sign in failed')));
+    } else {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => UserInformationProfile(googleUser: googleUser),
+      ));
+    }
 
     // Handle the result here
     if (googleUser != null) {
@@ -14,6 +26,7 @@ Future<void> signIn() async {
       // User canceled the sign-in process
       print('User canceled sign-in');
     }
+    return googleUser;
   } catch (e) {
     // Handle errors here
     print('Error during sign in: $e');
