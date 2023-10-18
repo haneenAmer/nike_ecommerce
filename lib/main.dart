3ce3d98221fe326b8f6afd6c4b4b6b4964ddb6d3
eng. Haneen Amer <89270722+haneenAmer@users.noreply.github.com>
2023-10-18 10:28:04 +0300

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nike_ecommerce/screens/signin.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
